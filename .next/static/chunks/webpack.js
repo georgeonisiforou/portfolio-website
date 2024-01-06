161d195df9eb7c099cccd5c712be7b469de3fde9
George Onisiforou <georgeonisiforou@Georges-MacBook-Pro.local>
2024-01-06 17:08:21 +0200

    styling fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33fd9fa9434abf26"; }
+/******/ 		__webpack_require__.h = function() { return "bcdf227c628ebe56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
